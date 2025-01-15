d954b7b32787bfba5181bfd960a1f88839885049
Babacar Diop <babacardiop1998@gmail.com>
2025-01-15 20:59:51 +0000

    Innitialisation de l'API du projet

---
+++ src/main/java/sn/forcen/java/groupe1/sousgroupe2/evoteapispring/EVoteApiSpringApplication.java
@@ -0,0 +1,13 @@
+package sn.forcen.java.groupe1.sousgroupe2.evoteapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVoteApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EVoteApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/forcen/java/groupe1/sousgroupe2/evoteapispring/EVoteApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.forcen.java.groupe1.sousgroupe2.evoteapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVoteApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
