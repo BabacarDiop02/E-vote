4195eccb0b90e747d15a965e6f853162590ddf80
Babacar Diop <145564743+BabacarDiop02@users.noreply.github.com>
2025-03-01 16:45:35 +0000

    gestion des candidat pour angular

--- src/main/java/sn/forcen/java/groupe1/sousgroupe2/evoteapispring/controller/CandidateController.java
+++ src/main/java/sn/forcen/java/groupe1/sousgroupe2/evoteapispring/controller/CandidateController.java
@@ -1,6 +1,9 @@
 package sn.forcen.java.groupe1.sousgroupe2.evoteapispring.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +11,8 @@ import org.springframework.web.multipart.MultipartFile;
 import sn.forcen.java.groupe1.sousgroupe2.evoteapispring.dto.CandidateDTO;
 import sn.forcen.java.groupe1.sousgroupe2.evoteapispring.service.CandidateService;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Set;
 
 @RestController
@@ -35,9 +40,25 @@ public class CandidateController {
         }
     }
 
+    @GetMapping("/files/{subDir}/{fileName}")
+    public ResponseEntity<Resource> getFile(@PathVariable String subDir, @PathVariable String fileName) throws IOException, IOException {
+        Resource file = this.candidateService.getFile(subDir, fileName);
+        // Détecter automatiquement le type MIME du fichier
+        String contentType = Files.probeContentType(file.getFile().toPath());
+
+        // Si le type est inconnu, mettre un type par défaut
+        if (contentType == null) {
+            contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE; // Type générique pour les fichiers binaires
+        }
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, contentType)
+                .body(file);
+    }
+
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     @PostMapping("/create-candidate")
-    public ResponseEntity<?> createCandidate(@RequestBody CandidateDTO candidateDTO, @RequestBody MultipartFile fileProgram, @RequestBody MultipartFile imageCandidate) {
+    public ResponseEntity<?> createCandidate(@RequestPart CandidateDTO candidateDTO, @RequestPart("fileProgram") MultipartFile fileProgram, @RequestPart("imageCandidate") MultipartFile imageCandidate) {
         try {
             CandidateDTO createdCandidateDTO = this.candidateService.createCandidate(candidateDTO, fileProgram, imageCandidate);
             return ResponseEntity.ok(createdCandidateDTO);
@@ -48,9 +69,13 @@ public class CandidateController {
 
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     @PutMapping(path = "/update-candidate")
-    public ResponseEntity<CandidateDTO> updateCandidate(@RequestBody CandidateDTO candidateDTO) {
-        CandidateDTO updatedCandidateDTO = this.candidateService.updateCandidate(candidateDTO);
-        return ResponseEntity.ok(updatedCandidateDTO);
+    public ResponseEntity<?> updateCandidate(@RequestPart CandidateDTO candidateDTO, @RequestPart("fileProgram") MultipartFile fileProgram, @RequestPart("imageCandidate") MultipartFile imageCandidate) {
+        try {
+            CandidateDTO updatedCandidateDTO = this.candidateService.updateCandidate(candidateDTO, fileProgram, imageCandidate);
+            return ResponseEntity.ok(updatedCandidateDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error add candidate !" + e.getMessage());
+        }
     }
 
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
